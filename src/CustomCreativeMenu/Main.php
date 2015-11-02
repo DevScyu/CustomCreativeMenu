e16f8bbc56b82542718c203c8cbd0427d5a587f1
DRedDog <dreddogpe@gmail.com>
2015-11-02 20:46:36 +0000

    Update Main.php

--- src/CustomCreativeMenu/Main.php
+++ src/CustomCreativeMenu/Main.php
@@ -15,57 +15,59 @@ class Main extends PluginBase implements Listener{
 	if(!is_dir($this->getDataFolder())){
 		@mkdir($this->getDataFolder());
 	}
-    $this->customCreativeMenu = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(
-      "added" => [
-		 246 => 0, #Glowing Obsidian
-		 255 => 0, #Grey "Glitch Stone"
-      ],
-      "removed" => [
-         46 => 0, #TNT
-		 7 => 0, #Bedrock
-      ],
-	  "Reactors" => true,
-	  "ReactorMeta" => [
-	  1, #Blue Reactor
-	  2, #Red Reactor
-	  ],
-	  "#False to remove all eggs",
-	  "Eggs" => true,
-	  "EggMeta" => [
-		  10,
-		  11,
-		  12,
-		  13,
-		  14,
-		  15,
-		  16,
-		  17,
-	    #  18,
-		#  19,
-		#  20,
-		#  21,
-		#  23,
-		#  24,
-		#  25,
-		#  26,
-		#  27,
-		#  28,
-		#  29,
-		#  30,
-		#  31,
-		  32,
-		  33,
-		  34,
-		  35,
-		  36,
-		  37,
-		  38,
-		  39,
-		  40,
-		  41,
-		  42,
-	  ],
-    )));
+	if(!file_exists($this->getDataFolder()."config.yml")){
+		$this->customCreativeMenu = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+		  "added" => [
+			 246 => 0, #Glowing Obsidian
+			 255 => 0, #Grey "Glitch Stone"
+		  ],
+		  "removed" => [
+			 46 => 0, #TNT
+			 7 => 0, #Bedrock
+		  ],
+		  "Reactors" => true,
+		  "ReactorMeta" => [
+		  1,
+		  2,
+		  ],
+		  "#False to remove all eggs",
+		  "Eggs" => true,
+		  "EggMeta" => [
+			  10,
+			  11,
+			  12,
+			  13,
+			  14,
+			  15,
+			  16,
+			  17,
+			#  18,
+			#  19,
+			#  20,
+			#  21,
+			#  23,
+			#  24,
+			#  25,
+			#  26,
+			#  27,
+			#  28,
+			#  29,
+			#  30,
+			#  31,
+			  32,
+			  33,
+			  34,
+			  35,
+			  36,
+			  37,
+			  38,
+			  39,
+			  40,
+			  41,
+			  42,
+		  ],
+		)));
+	}
 	if($this->customCreativeMenu->get("Eggs") === false){
 		foreach($this->customCreativeMenu->get("EggMeta") as $damagevalue){
 		Item::removeCreativeItem(Item::get(383, $damagevalue)); 
@@ -84,6 +86,7 @@ class Main extends PluginBase implements Listener{
 		Item::addCreativeItem(Item::get(247, $damagevalue));  
 		}
 	}
+	
     foreach($this->customCreativeMenu->get("added") as $aitem => $damagevalue){
       Item::addCreativeItem(Item::get($aitem, $damagevalue));
 	  $this->getLogger()->info($aitem . ":" . $damagevalue);
